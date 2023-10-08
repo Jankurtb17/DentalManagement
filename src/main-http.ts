6dbf522d66e5c9f7cd61e11b72ddd5d20ac3a624
kurt2 <j.kurtbayaras@gmail.com>
2023-10-08 18:22:30 +0800

    refactor: add withCredentials

--- src/main-http.ts
+++ src/main-http.ts
@@ -1,11 +1,12 @@
 import axios, { type AxiosInstance  } from "axios";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: "https://dental-management-backend.vercel.app",
-  // baseURL: "http://localhost:8080",
+  // baseURL: "https://dental-management-backend.vercel.app",
+  baseURL: "http://localhost:8080",
   headers: {
     "Content-type": "application/json",
   },
+  withCredentials: true,
 });
 
 export default apiClient;
